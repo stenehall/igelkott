83919165d14bb7338495f523152a22c1b75de0da
Johan Stenehall <stenehall@gmail.com>
2013-11-21 21:06:12 +0100

    Cleaning up the connect plugin

--- plugins/connect.js
+++ plugins/connect.js
@@ -10,14 +10,18 @@ var Connect = function Connect() {
 
 Connect.prototype.connect = function connect ()
 {
-  this.igelkott.push({command: 'NICK', parameters: [this.igelkott.config.server.nick]});
+  var nick = {
+    command: 'NICK',
+    parameters: [this.igelkott.config.server.nick]
+  };
 
-  var obj = {
+  var user = {
     'command': 'USER',
     'parameters': [ this.igelkott.config.server.nick, '0', '*', ':'+this.igelkott.config.server.nick ] // This is just stupid stupid stupid. Stupid, stupid stupid. Stupid...
   };
 
-  this.igelkott.push(obj);
+  this.igelkott.push(nick);
+  this.igelkott.push(user);
 };
 
 exports.Plugin = Connect;
