324626fe2869355c2d602d60ec0cb8c26b3a74ff
Johan Stenehall <stenehall@gmail.com>
2014-02-12 17:57:42 +0100

    Added support for PASS

--- plugins/connect.js
+++ plugins/connect.js
@@ -15,6 +15,15 @@ Connect.prototype.connect = function connect ()
     parameters: [this.igelkott.config.server.nick]
   };
 
+  if (this.igelkott.config.server.password !== undefined)
+  {
+    var pass = {
+      command: 'PASS',
+      parameters: [this.igelkott.config.server.password]
+    };
+    this.igelkott.push(pass);
+  }
+
   var user = {
     'command': 'USER',
     'parameters': [ this.igelkott.config.server.nick, '0', '*', ':'+this.igelkott.config.server.nick ] // This is just stupid stupid stupid. Stupid, stupid stupid. Stupid...
